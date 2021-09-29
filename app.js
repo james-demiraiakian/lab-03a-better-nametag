71ae910ea4b91a111b30e60939a06434944294db
James Demiraiakian <james.armen@gmail.com>
2021-09-29 14:40:57 -0700

    finished

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 // import functions and grab DOM elements
+//Name Section
 const nameInput = document.getElementById('name');
-const button = document.getElementById('generate');
+const nameButton = document.getElementById('generate');
 const nameTag = document.getElementById('first-name');
 
-// initialize global state
+//Pronouns Section
+const proIn = document.getElementById('pronouns');
+const proButton = document.getElementById('set');
+const pronoun = document.getElementById('proDisplay');
 
+//Color Section
+const col1 = document.getElementById('color1');
+const col2 = document.getElementById('color2');
+const col3 = document.getElementById('color3');
+let headerColor = document.getElementById('head');
+let footerColor = document.getElementById('foot');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
-
-button.addEventListener('click', ()=> {
+//Name Event Listener
+nameButton.addEventListener('click', ()=> {
     let newName = nameInput.value;
     nameTag.textContent = newName;
+});
+
+//Pronoun Event Listener
+proButton.addEventListener('click', ()=> {
+    let newPro = proIn.value;
+    pronoun.textContent = newPro;
+});
+
+//Color Event Listeners
+col1.addEventListener('click', ()=> {
+    headerColor.style.backgroundColor = '#904FCE';
+    footerColor.style.backgroundColor = '#904FCE';
+});
+
+col2.addEventListener('click', ()=> {
+    headerColor.style.backgroundColor = '#048BA8';
+    footerColor.style.backgroundColor = '#048BA8';
+});
+
+col3.addEventListener('click', ()=> {
+    headerColor.style.background = '#16DB93';
+    footerColor.style.background = '#16DB93';
 });
